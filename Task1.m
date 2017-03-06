a6e35e5215da7640a21f1b358e3cae2a02d7f0d9
BuildTools <unconfigured@null.spigotmc.org>
2017-03-06 10:31:14 +0800

    commit

--- Task1.m
+++ Task1.m
@@ -1,14 +1,18 @@
 clear; 
 
-[wave,fs]=audioread('HaydnL.wav');
-wavef = wave(:,1)+wave(:,2)/2;
-t=0:1/fs:(length(wavef)-1)/fs;
-figure;
+k=32;
 
+[wave,fs]=audioread('HaydnL.wav');
+
+waveT = (wave(:,1)+wave(:,2))/2;
+wavef = waveT(1:k:end); 
+
+sound(wavef,fs/k);
+
+t=0:1/(fs/k):(length(wavef)-1)/(fs/k);
+figure;
 plot(t,wavef);
 
 xlabel('Time(s)');
 ylabel('Amplitude');
 title('Wavelength of HaydnL.wav');
-
-succ
